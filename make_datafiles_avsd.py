832bb3d3d638c4289e1ce023b31683c80af9650c
CHEN Wang <wchen@cse.cuhk.edu.hk>
2020-02-21 18:28:42 +0800

    add process code for avsd and samsum

---
+++ make_datafiles_avsd.py
@@ -0,0 +1,44 @@
+import os
+import argparse
+
+from utils import write_to_tar, init_logger
+
+
+if __name__ == '__main__':
+    dataset = "AVSD"
+    parser = argparse.ArgumentParser(description="make_datafiles_{}.py".format(dataset))
+    parser.add_argument("--data_path", "-data_path", type=str,
+                        default="/home/rickywchen/code/DialogueSum/dataset/{}/1_preprocessed_from_raw".format(dataset))
+    parser.add_argument("--save_path", "-save_path", type=str,
+                        default="/home/rickywchen/code/DialogueSum/dataset/{}/3_cnn_daily_style_processed".format(dataset))
+    parser.add_argument("--min_src_len", "-min_src_len", type=int, default=15)
+    parser.add_argument("--min_sum_len", "-min_sum_len", type=int, default=1)
+    parser.add_argument("--log_file", "-log_file", type=str,
+                        default="logs/{}_cnn_daily_style_process_log.txt".format(dataset))
+    opts = parser.parse_args()
+
+    # Create some new directories
+    if not os.path.exists(opts.save_path):
+        os.makedirs(opts.save_path)
+    if not os.path.exists(os.path.dirname(opts.log_file)):
+        os.makedirs(os.path.dirname(opts.log_file))
+
+    init_logger(opts.log_file)
+
+    # Read the tokenized stories, do a little postprocessing
+    # then write to bin files
+    write_to_tar(
+        dial_file=os.path.join(opts.data_path, 'AVSD_dialogue_minSumLen5_srcTrunc400_tgtTrunc100_spacy_test.txt'),
+        sum_file=os.path.join(opts.data_path, 'AVSD_summary_minSumLen5_srcTrunc400_tgtTrunc100_spacy_test.txt'),
+        out_file=os.path.join(opts.save_path, "test.tar"))
+    write_to_tar(
+        dial_file=os.path.join(opts.data_path, 'AVSD_dialogue_minSumLen5_srcTrunc400_tgtTrunc100_spacy_valid.txt'),
+        sum_file=os.path.join(opts.data_path, 'AVSD_summary_minSumLen5_srcTrunc400_tgtTrunc100_spacy_valid.txt'),
+        out_file=os.path.join(opts.save_path, "val.tar"))
+    write_to_tar(
+        dial_file=os.path.join(opts.data_path, 'AVSD_dialogue_minSumLen5_srcTrunc400_tgtTrunc100_spacy_train.txt'),
+        sum_file=os.path.join(opts.data_path, 'AVSD_summary_minSumLen5_srcTrunc400_tgtTrunc100_spacy_train.txt'),
+        out_file=os.path.join(opts.save_path, "train.tar"),
+        makevocab=True,
+        min_src_len=opts.min_src_len,
+        min_sum_len=opts.min_sum_len)
---
+++ utils.py
@@ -0,0 +1,96 @@
+import os
+import collections
+import logging
+
+import json
+import tarfile
+import io
+import pickle as pkl
+
+
+UTR_SPLITTER = '|'
+
+logger = logging.getLogger()
+
+
+def init_logger(log_file=None, log_file_level=logging.NOTSET):
+    log_format = logging.Formatter("[%(asctime)s %(levelname)s] %(message)s")
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(log_format)
+    logger.handlers = [console_handler]
+
+    if log_file and log_file != '':
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(log_file_level)
+        file_handler.setFormatter(log_format)
+        logger.addHandler(file_handler)
+
+    return logger
+
+
+def write_to_tar(dial_file, sum_file, out_file, makevocab=False, min_src_len=0, min_sum_len=0):
+    """Reads the tokenized dialogue file and summary file writes them as the story and abstract to a out_file.
+    """
+    logger.info("Making bin file for {} and {}...".format(dial_file, sum_file))
+
+    if makevocab:
+        vocab_counter = collections.Counter()
+
+    dial_lines = open(dial_file, encoding='utf-8').readlines()
+    sum_lines = open(sum_file, encoding='utf-8').readlines()
+    assert len(dial_lines) == len(sum_lines), "The number of dialogues should be the same with the number of the summaries."
+    num_stories = len(dial_lines)
+
+    logger.info("Total number of data: {}".format(num_stories))
+
+    with tarfile.open(out_file, 'w') as writer:
+        for idx, (dial, summ) in enumerate(zip(dial_lines, sum_lines)):
+            if idx % 1000 == 0:
+                logger.info("Writing story {} of {}; {:.2f} percent done".format(
+                    idx, num_stories, float(idx)*100.0/float(num_stories)))
+            # filter the data sample whose dialogue length is less than min_src_len
+            if min_src_len > 0 and len(dial.strip().split()) < min_src_len:
+                continue
+            # filter the data sample whose summary length is less than min_sum_len
+            if min_sum_len > 0 and len(summ.strip().split()) < min_sum_len:
+                continue
+
+            # Get the strings to write to .bin file
+            # article_sents, abstract_sents = get_art_abs(story_file)
+            article_sents = [line.strip() for line in dial.split(UTR_SPLITTER) if len(line.strip()) != 0]
+            abstract_sents = [summ.strip()]
+
+            # Write to JSON file
+            js_example = {}
+            js_example['id'] = idx
+            js_example['article'] = article_sents
+            js_example['abstract'] = abstract_sents
+            js_serialized = json.dumps(js_example, indent=4).encode()
+            save_file = io.BytesIO(js_serialized)
+            tar_info = tarfile.TarInfo('{}/{}.json'.format(
+                os.path.basename(out_file).replace('.tar', ''), idx))
+            tar_info.size = len(js_serialized)
+            writer.addfile(tar_info, save_file)
+
+            # Write the vocab to file, if applicable
+            if makevocab:
+                art_tokens = ' '.join(article_sents).split()
+                abs_tokens = ' '.join(abstract_sents).split()
+                tokens = art_tokens + abs_tokens
+                tokens = [t.strip() for t in tokens] # strip
+                tokens = [t for t in tokens if t != ""] # remove empty
+                vocab_counter.update(tokens)
+
+    logger.info("Finished writing file {}\n".format(out_file))
+
+    # write vocab to file
+    if makevocab:
+        logger.info("Writing vocab file...")
+        with open(os.path.join(os.path.dirname(out_file), "vocab_cnt.pkl"),
+                  'wb') as vocab_file:
+            pkl.dump(vocab_counter, vocab_file)
+        logger.info("Finished writing vocab file")
+
